0b694e6df8be1509f6688fe1ac02d4269d69f2e8
HugoGarcia2005 <hugo.garcia3075@alumnos.udg.mx>
2025-05-11 14:48:58 -0600

    Add files via upload

---
+++ Principal Fase 2/Branch.v
@@ -0,0 +1,10 @@
+module Branch(
+	input Zero_Flag,
+	input Branch_UC,
+	output reg Salida_MUXPC
+);
+
+always @* begin
+	Salida_MUXPC = Zero_Flag & Branch_UC;
+end
+endmodule
---
+++ Principal Fase 2/MultiplexorTipoI.v
@@ -0,0 +1,18 @@
+module MultiplexorTipoI (
+	input [4:0] InstruccionRT,
+	input [4:0] InstruccionRD,
+	input RegDst,
+	output reg [31:0] W_register_BR
+);
+
+always @* begin
+	if (RegDst) begin
+		W_register_BR <= InstruccionRD;
+	end
+	else begin
+		W_register_BR <= InstruccionRT;
+	end
+end
+
+endmodule
+
